4dcf577aae11a7ca9585408c92b53ed98300ad4d
NicolasG12 <naguevar@ucsc.edu>
2023-02-21 16:02:07 -0800

    Updated some of the enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Enemy : Entity {
+public abstract class Enemy : Entity
+{
     [Header("Movement Variables")]
     [SerializeField] protected int direction = 0;
     [SerializeField] private float _distance = 10f;
@@ -15,22 +16,29 @@ public abstract class Enemy : Entity {
     private bool _stunned = false;
 
 
-    protected override void Awake() {
+    protected override void Awake()
+    {
         hp = maxHP;
         rb = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    public void FixedUpdate() {
+    public void Update()
+    {
         float dist = Vector3.Distance(transform.position, player.transform.position);
-        if (dist < _distance) {
-            if (transform.position.x < player.transform.position.x) { direction = 1; } else if (transform.position.x > player.transform.position.x) { direction = -1; }
+        if (dist < _distance)
+        {
+            direction = transform.position.x < player.transform.position.x ? -1 : 1;
         }
-        if(!_stunned) { 
+        if (!_stunned)
+        {
             rb.velocity = new Vector2(speed * direction, 0);
-            if (rb.velocity.x < 0) {
+            if (rb.velocity.x < 0)
+            {
                 _spriteRenderer.flipX = false;
-            } else {
+            }
+            else
+            {
                 _spriteRenderer.flipX = true;
             }
         }
@@ -38,31 +46,39 @@ public abstract class Enemy : Entity {
 
     }
 
-    public void TakeDamage(Weapon weapon) {
+    public void TakeDamage(Weapon weapon)
+    {
         hp -= weapon.GetDmg();
         _stunned = true;
-        if (hp <= 0) {
+        if (hp <= 0)
+        {
             this.gameObject.SetActive(false);
-        } else {
+        }
+        else
+        {
             rb.AddForce(new Vector2(weapon.GetKnockback(), 0) * (-direction), ForceMode2D.Impulse);
             StartCoroutine(Stunned());
         }
     }
 
-    IEnumerator Stunned() {
+    IEnumerator Stunned()
+    {
         yield return new WaitForSeconds(1);
-        _stunned= false;
+        _stunned = false;
     }
 
 
-    public override void Attack() {
+    public override void Attack()
+    {
         Player p = player.GetComponent<Player>();
         p.SetHP(p.GetHP() - damage);
     }
 
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Player") {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
             Attack();
         }
     }

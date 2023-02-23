91605084637959f3f6d1b00d995e2ee1f8e2958e
NicolasG12 <naguevar@ucsc.edu>
2023-02-22 16:06:06 -0800

    Put in drawer sprite

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,43 +4,49 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    [Header("Stats:")]
-    [SerializeField] protected float damage;
-    [SerializeField] protected float knockback;
+   [Header("Stats:")]
+   [SerializeField] protected float damage;
+   [SerializeField] protected float knockback;
 
-    private Animator anim;
+   private Animator anim;
+   private Animation a;
 
-    private bool attacking = false;
+   private bool attacking = false;
 
-    public float GetKnockback() { return knockback; }
-    public float GetDmg() { return damage; }
-    private void Awake() {
-        anim = GetComponent<Animator>();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public float GetKnockback() { return knockback; }
+   public float GetDmg() { return damage; }
+   private void Awake() {
+      anim = GetComponent<Animator>();
+   }
+   // Start is called before the first frame update
+   void Start() {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   }
 
-    public void Attack() {
-        anim.CrossFadeInFixedTime("attack", 0.1f);
-        attacking = true;
-    }
+   // Update is called once per frame
+   void Update() {
 
-    private void OnTriggerStay2D(Collider2D other) {
-        if(other.tag == "Enemy") {
-            Enemy enemy = other.gameObject.GetComponent<Enemy>();
-            if (attacking) {
-                enemy.TakeDamage(this);
-                attacking = false;
-            }
-        }
-    }
+   }
+
+   public void Attack() {
+      if (!attacking) {
+         anim.CrossFadeInFixedTime("attack", 0.1f);
+         attacking = true;
+         // StartCoroutine(Attacking());
+      }
+   }
+
+   private void OnTriggerStay2D(Collider2D other) {
+      if (other.tag == "Enemy") {
+         Enemy enemy = other.gameObject.GetComponent<Enemy>();
+         if (attacking) {
+            enemy.TakeDamage(this);
+         }
+      }
+   }
+
+   // IEnumerator Attacking() {
+   //    // yield return new WaitForSeconds(anim.);
+   //    attacking = false;
+   // }
 }
